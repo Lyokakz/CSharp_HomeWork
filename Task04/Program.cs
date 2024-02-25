bcc051331299150188e780465bb90bd62053cf83
Lyokakz <lenusik__rybka@mail.ru>
2024-02-25 09:36:49 +0600

    Add SimpleAlgorithms:Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Напишите программу, которая на вход принимает натуральное число N, 
+а на выходе показывает его цифры через запятую.
+*/
+
+Console.WriteLine("Введите натуральное число N:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string digits = "";
+int newNumber = number;
+
+while (newNumber > 0)
+{
+    int digit = newNumber % 10;
+    digits = digit + (digits == "" ? "" : ",") + digits;
+    newNumber /= 10;
+}
+
+Console.WriteLine($"{number} => {digits}");
+
+
+
